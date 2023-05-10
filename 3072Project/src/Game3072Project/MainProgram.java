6666f17f397e41b392d508b325aecb2d2d73499e
ccronk25 <112973833+ccronk25@users.noreply.github.com>
2023-05-10 16:58:25 -0500

    Add 3072 project

---
+++ 3072Project/src/Game3072Project/MainProgram.java
@@ -0,0 +1,20 @@
+
+package Game3072Project;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+
+public class MainProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HomePage hp = new HomePage();
+        hp.setVisible(true);
+        hp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+    }
+}
